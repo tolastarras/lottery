4d235a3322f146cafdd08fcdc54c9a929d060566
Rafael <tolastarras@gmail.com>
2018-10-01 18:10:23 -0400

    initial setup

--- public_html/main.js
+++ public_html/main.js
@@ -3,9 +3,65 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-isNumberInArray = (numbers, number) => {
-    console.log('HERE:', numbers);
-    console.log('LEN:', numbers.length);
+
+const MIN = 1;
+const MAX = 49;
+const numberOfElements = 6;
+
+let numbers = [];
+let output = '';
+for (let i = 0; i < numberOfElements; i++) {
+    output += '<input id="' + i + '" type="text" maxlength="2" class="number">';
+}
+
+document.getElementById('app').innerHTML = output;
+
+// number fields
+let inputs = document.querySelectorAll('.number');
+
+// listen for events
+inputs.forEach(input => {
+    input.addEventListener('blur', e => {
+        console.log('value: ', e.target.value);
+        if (e.target.value < "1" || e.target.value > "49") {
+            document.getElementById(e.target.id).classList.add('invalid');
+        }
+
+        let myNumber = e.target.value;
+        console.log(myNumber);
+        console.log(numbers);
+        let isInArray = isNumberInArray(numbers, myNumber);
+        console.log('in array? ', isInArray);
+
+        if (!isInArray) {
+            numbers[e.target.id] = myNumber;
+            document.querySelector('.error-message').innerHTML = '';
+            document.getElementById(e.target.id).classList.remove('invalid');
+        } else if (myNumber === '') {
+            document.querySelector('.error-message').innerHTML = 'Empty value in cell ' + e.target.id;
+        } else {
+            document.querySelector('.error-message').innerHTML = myNumber + ' is already in the array';
+            document.getElementById(e.target.id).value = '';
+        }
+
+        console.log(numbers);
+    });
+});
+
+format = value => {
+    console.log('value: ', value);
+}
+
+isValid = value => {
+    let number = Number.parseInt(value);
+    if (!value || value < MIN || value > MAX) return false;
+    
+    return true;
+}
+
+isNumberInArray = number => {
+    console.log("My Number", number);
+    console.log("Numbers: ", numbers);
     // do not consider the last array element
     for (let i = 0; i < numbers.length; i++) {
         if (number === numbers[i] && i !== 5) {
